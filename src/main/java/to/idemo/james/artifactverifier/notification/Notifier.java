e8f16a78e5c7d378975045c0adcd96e8b0eb1734
James Idemoto <james.idemoto@gmail.com>
2022-10-29 16:24:33 -0700

    Adds javadoc

--- src/main/java/to/idemo/james/artifactverifier/notification/Notifier.java
+++ src/main/java/to/idemo/james/artifactverifier/notification/Notifier.java
@@ -5,6 +5,23 @@ package to.idemo.james.artifactverifier.notification;
  * Spring graph will be invoked when a verification check is failed.
  */
 public interface Notifier {
+    /**
+     * Triggered when an event of note occurs (e.g. changes to an external dependency out of our control fails validation)
+     * @param artifactName The name of the artifact
+     * @param artifactType The artifact type
+     * @param artifactLocation The location
+     * @param attributedUser The user that triggered the repo change
+     * @param reason Validation failure reason
+     */
     void warn(String artifactName, String artifactType, String artifactLocation, String attributedUser, String reason);
+
+    /**
+     * Triggered when a validation failure occurs (e.g. unsigned artifact detected)
+     * @param artifactName The name of the artifact
+     * @param artifactType the artifact type
+     * @param artifactLocation the location
+     * @param attributedUser The user that triggered the repo change
+     * @param reason Validation failure reason
+     */
     void alert(String artifactName, String artifactType, String artifactLocation, String attributedUser, String reason);
 }
